ffe6bb4405401b5dce0275b9cfddf30c5cd8a025
William Barrero <williama.barreroc@unac.edu.co>
2025-05-12 21:55:37 -0500

    ✨ Add HomeButton component and integrate it into CreatePostPage and Profile pages

--- ClientRenta/src/pages/CreatePostPage.jsx
+++ ClientRenta/src/pages/CreatePostPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import CreateForm from '../components/CreateForm'
+import HomeButton from '../components/HomeButton'
 
 const CreatePostPage = () => {
   return (
     <>
+      <HomeButton></HomeButton>
       <CreateForm></CreateForm>
 
     </>
